eee454c64ffc2e1d0d65de76e1112eab0d962f64
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-01-23 22:35:48 +0530

    Update index.js changing cores comment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cors({
   origin: 'https://stock-market-prediction-theta.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
