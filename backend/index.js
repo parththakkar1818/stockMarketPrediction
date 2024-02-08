0f564dcd503a8a75dd9942a745f579da3f093d6f
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-02-08 22:31:40 +0530

    Update index.js news part added

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const yfinance = require("yahoo-finance");
 const { log } = require("console");
 require("dotenv").config(); // Load environment variables from .env file
+const { LocalStorage } = require("node-localstorage");
+const localStorage = new LocalStorage("./scratch");
+
+const { PythonShell } = require("python-shell");
 const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
+const newsApiKey = process.env.ALPHA_VANTAGE_NEWS_API_KEY;
 
 const app = express();
 app.use(express.static("public"));
@@ -38,46 +43,49 @@ app.post("/getStockData", async (req, res) => {
   }
 });
 
-app.get("/alpha", (req, res) => {
-  "use strict";
-  var request = require("request");
+app.post("/getnewsrapidapi", async (req, res) => {
+  const axios = require("axios");
 
-  var url =
-    "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=ved&apikey=" +
-    apiKey;
-
-  var url2 =
-    "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=" +
-    apiKey;
-
-  request.get(
-    {
-      url: url,
-      json: true,
-      headers: { "User-Agent": "request" },
+  const options = {
+    method: "GET",
+    url: process.env.NEWS_API,
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+      "X-RapidAPI-Host": process.env.RAPIDAPI_HOST,
     },
-    (err, response, data) => {
-      if (err) {
-        console.log("Error:", err);
-      } else if (response.statusCode !== 200) {
-        console.log("Status:", response.statusCode);
-      } else {
-        // Check if data and bestMatches are defined
-        console.log(data);
-        if (data) {
-          const nseStocks = data.bestMatches.filter((stock) => {
-            return (
-              stock["4. region"] === "India/Bombay" &&
-              stock["3. type"] === "Equity"
-            );
-          });
+  };
 
-          console.log(nseStocks);
-          res.send(nseStocks);
-        }
-      }
+  try {
+    const response = await axios.request(options);
+    res.json({ news: response.data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
+
+app.post("/getnews", async (req, res) => {
+  try {
+    const apiKey = process.env.FINANCIALMODELAPI; // Replace with your API key
+    const page = Math.floor(Math.random() * 10) + 1;
+
+    const apiUrl = `https://financialmodelingprep.com/api/v3/fmp/articles?page=${page}&apikey=${apiKey}`;
+    
+    const response = await fetch(apiUrl);
+
+    if (response.ok) {
+      const data = await response.json();
+      res.status(200).json({ success: true, news: data });
+      // console.log(data);
+    } else {
+      const errorData = await response.json();
+      res.status(response.status).json({ success: false, error: errorData });
+      console.log(errorData);
     }
-  );
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({ success: false, error: "Error fetching news" });
+  }
 });
 
 app.post("/predictstock/:startdate/:enddate/:stocksymbol", async (req, res) => {
@@ -91,7 +99,7 @@ app.post("/predictstock/:startdate/:enddate/:stocksymbol", async (req, res) => {
 
   try {
     const combinedArgs = [startDate, endDate, stockSymbol].join(",");
-    console.log("I am from backend",combinedArgs);
+    // console.log("I am from backend", combinedArgs);
     const pythonProcess = spawn("python", ["get_stockdata.py", combinedArgs]);
 
     let pythonOutput = "";
@@ -100,9 +108,6 @@ app.post("/predictstock/:startdate/:enddate/:stocksymbol", async (req, res) => {
     pythonProcess.stdout.on("data", (data) => {
       if (data.toString()[0] == "[" && data.toString()[1] == "[") {
         pythonOutput += data.toString();
-        // console.log(data);
-        // console.log(data.toString());
-        // console.log(data.toString()[0], data.toString()[1]);
       }
     });
 
@@ -120,8 +125,7 @@ app.post("/predictstock/:startdate/:enddate/:stocksymbol", async (req, res) => {
           inner.split(",").map(Number)
         );
         // console.log(parsedArray);
-        res.json({ success: true, predictionDataInJSON: parsedArray});
-
+        res.json({ success: true, predictionDataInJSON: parsedArray });
       } else {
         res.status(500).send("Error running the Python script");
       }
@@ -130,6 +134,47 @@ app.post("/predictstock/:startdate/:enddate/:stocksymbol", async (req, res) => {
     console.error(error);
   }
 });
+
+app.get("/alpha", (req, res) => {
+  "use strict";
+  var request = require("request");
+
+  var url =
+    "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=ved&apikey=" +
+    apiKey;
+
+  var url2 =
+    "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=" +
+    apiKey;
+
+  var topics = "financial_markets"; // Add more topics as needed
+
+  var url3 =
+    "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=" +
+    topics +
+    "&apikey=" +
+    newsApiKey;
+
+  request.get(
+    {
+      url: url3,
+      json: true,
+      headers: { "User-Agent": "request" },
+    },
+    (err, response, data) => {
+      if (err) {
+        console.log("Error:", err);
+      } else if (response.statusCode !== 200) {
+        console.log("Status:", response.statusCode);
+      } else {
+        // Check if data and bestMatches are defined
+        console.log(data);
+        res.send(data);
+      }
+    }
+  );
+});
+
 app.listen(3001, () => {
   console.log("started on 3001...");
 });
